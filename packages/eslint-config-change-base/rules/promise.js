c6d374052a83dd81922217843c27e8abfd93cf31
Chris Campbell <chris@change.org>
2017-01-30 14:16:34 -0800

    Treat these promise warnings as errors

--- packages/eslint-config-change-base/rules/promise.js
+++ packages/eslint-config-change-base/rules/promise.js
@@ -4,14 +4,14 @@ module.exports = {
   ],
 
   rules: {
-    'promise/always-return': 'warn',
+    'promise/always-return': 'error',
     'promise/no-return-wrap': 'error',
     'promise/param-names': 'error',
     'promise/catch-or-return': 'error',
     'promise/no-native': 'off',
-    'promise/no-nesting': 'warn',
-    'promise/no-promise-in-callback': 'warn',
-    'promise/no-callback-in-promise': 'warn',
-    'promise/avoid-new': 'warn',
+    'promise/no-nesting': 'error',
+    'promise/no-promise-in-callback': 'error',
+    'promise/no-callback-in-promise': 'error',
+    'promise/avoid-new': 'error',
   },
 };
