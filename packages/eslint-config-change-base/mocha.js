0390a8efee526601e034842f308e9c39911461b1
Chris Campbell <chris@change.org>
2018-11-08 17:19:21 -0800

    Enforce new/stricter lint checks for mocha and promises

--- packages/eslint-config-change-base/mocha.js
+++ packages/eslint-config-change-base/mocha.js
@@ -1,14 +1,19 @@
 module.exports = {
-  env: {
-    mocha: true,
-  },
-
   plugins: [
     'mocha',
   ],
 
+  env: {
+    mocha: true,
+  },
+
   rules: {
+    'mocha/handle-done-callback': 'error',
     'mocha/no-exclusive-tests': 'error',
+    'mocha/no-identical-title': 'error',
+    'mocha/no-nested-tests': 'error',
+    'mocha/no-return-and-callback': 'error',
     'mocha/no-skipped-tests': 'error',
+    'mocha/no-synchronous-tests': 'error',
   },
 };
--- packages/eslint-config-change-base/promise.js
+++ packages/eslint-config-change-base/promise.js
@@ -9,9 +9,12 @@ module.exports = {
     'promise/catch-or-return': 'error',
     'promise/no-callback-in-promise': 'error',
     'promise/no-native': 'off',
+    'promise/no-new-statics': 'error',
     'promise/no-nesting': 'error',
     'promise/no-promise-in-callback': 'error',
+    'promise/no-return-in-finally': 'error',
     'promise/no-return-wrap': 'error',
     'promise/param-names': 'error',
+    'promise/valid-params': 'error',
   },
 };
