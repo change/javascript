96de2b5f6ba5990f7491642c137c9b53ef27d289
Karl Varga <kjvarga@gmail.com>
2021-03-15 10:17:14 -0700

    Codemod for converting tap to when

---
+++ packages/change-codemod/transforms/tap-to-when.js
@@ -0,0 +1,36 @@
+// Convert tap to then in promise chains.
+//
+// Known issues: Doesn't support inline expressions unless omitting the return statement.
+//
+// Pass option --omit-return to omit the return in the translated tap call.
+// This is useful for test files, where tap is often the last call in the chain.
+export default function transformer(file, api, options) {
+  const omitReturn = options['omit-return'];
+  const j = api.jscodeshift;
+  const root = j(file.source);
+  const tapCalls = root.find(j.CallExpression, {
+    callee: {
+      property: {
+        name: 'tap',
+      },
+    },
+  });
+
+  tapCalls.forEach((p) => {
+    p.node.callee.property.name = 'then';
+    const argumentName = p.node.arguments[0].params.length ? p.node.arguments[0].params[0].name : 'result';
+    const isLastInChain = !p.parentPath.value.property;
+
+    // Add an argument to the lambda function if there is none
+    if ((!isLastInChain || !omitReturn) && p.node.arguments[0].params.length === 0) {
+      p.node.arguments[0].params.push('result');
+    }
+
+    if (omitReturn && !isLastInChain) return;
+
+    // Add the return statement
+    p.node.arguments[0].body.body.push(j.returnStatement(j.identifier(argumentName)));
+  });
+  return root.toSource();
+}
+
