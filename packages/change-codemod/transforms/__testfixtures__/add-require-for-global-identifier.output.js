67f3dcea01fc8fd648d67718ff7f69164ae1c62c
Karl Varga <kjvarga@gmail.com>
2021-04-01 18:07:25 -0700

    Add new transform for adding require statement for global identifier

---
+++ packages/change-codemod/transforms/__testfixtures__/add-require-for-global-identifier.output.js
@@ -0,0 +1,11 @@
+const sinon = require("sinon");
+const x = require('something');
+
+// sinon in this comment should be ignored
+context('when called in the context of a test function', () => {
+      it('calls the handler, passing in the attempt count', function () {
+        const handler = sinon.stub();
+        getRetryFunction(handler).call(this);
+        expect(handler).to.have.been.calledWith(sinon.match({ attemptCount: 1 }));
+      });
+    });
---
+++ packages/change-codemod/transforms/add-require-for-global-identifier.js
@@ -0,0 +1,26 @@
+// When an identifier is detected in a file, add a require (with the identifier name) to the
+// top of the file.  Useful when needing to add requires for global identifiers.
+//
+// The identifier must be specified using the --identifier option, e.g. --identifier=sinon
+export default function transformer(file, api, options) {
+  let { identifier } = options;
+  if (!identifier) throw new Error("Please specify the name of the identifier name you would like to detect using the --identifier option.");
+
+  const j = api.jscodeshift;
+  const root = j(file.source);
+  const found =
+    root
+      .find(j.Identifier, {
+        name: identifier
+      })
+      .size() > 0;
+
+  if (found) {
+    // Add a require for the identifier to the top of the file
+    const variableDec = j.variableDeclaration("const", [j.variableDeclarator(j.identifier(identifier), j.callExpression(j.identifier("require"), [j.literal(identifier)]))]);
+    root.find(j.Program).forEach((p) => {
+      p.value.body.unshift(variableDec);
+    });
+  }
+  return root.toSource();
+}
