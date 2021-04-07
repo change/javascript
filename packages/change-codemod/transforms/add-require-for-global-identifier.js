6490d8afe62988a24c8fb4c47066699e48d08647
Jose Gordillo <jgordillo@change.org>
2021-04-07 11:54:49 -0700

    renaming to packageName since package is a reserved word

--- packages/change-codemod/transforms/add-require-for-global-identifier.js
+++ packages/change-codemod/transforms/add-require-for-global-identifier.js
@@ -2,19 +2,19 @@
 // top of the file.  Useful when needing to add requires for global identifiers.
 //
 // The identifier must be specified using the --identifier option, e.g. --identifier=sinon
-// The argument for the new require statement can be specified separately from the targeting identifier, e.g. --identifier=_ --package=lodash
+// The argument for the new require statement can be specified separately from the targeting identifier, e.g. --identifier=_ --packageName=lodash
 export default function transformer(file, api, options) {
   let { identifier } = options;
   if (!identifier) throw new Error("Please specify the name of the identifier name you would like to detect using the --identifier option.");
 
-  const package = options.package || identifier;
+  const packageName = options.packageName || identifier;
   const j = api.jscodeshift;
   const root = j(file.source);
 
   const alreadyHasRequireDeclaration =
     root
       .find(j.CallExpression, { callee: { name: 'require' } })
-      .filter(n => n.node.arguments.length >= 1 && n.node.arguments[0].value === package)
+      .filter(n => n.node.arguments.length >= 1 && n.node.arguments[0].value === packageName)
       .size() > 0;
 
   const found =
@@ -27,7 +27,7 @@ export default function transformer(file, api, options) {
 
   if (found) {
     // Add a require for the identifier to the top of the file
-    const variableDec = j.variableDeclaration("const", [j.variableDeclarator(j.identifier(identifier), j.callExpression(j.identifier("require"), [j.literal(package)]))]);
+    const variableDec = j.variableDeclaration("const", [j.variableDeclarator(j.identifier(identifier), j.callExpression(j.identifier("require"), [j.literal(packageName)]))]);
     root.find(j.Program).forEach((p) => {
       p.value.body.unshift(variableDec);
     });
