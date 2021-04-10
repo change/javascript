a08b31d74eaa7b9826af7b617783f377e7e7c00b
Karl Varga <kjvarga@gmail.com>
2021-04-09 18:01:09 -0700

    Add transform to convert lodash fp to curried syntax

---
+++ packages/change-codemod/transforms/__testfixtures__/convert-lodash-to-curried-syntax.output.js
@@ -0,0 +1,21 @@
+fp.getOr('')('raw.message');(err);
+fp.get('raw.decline_code')(err);;
+fp.get('raw.decline_code')(err);
+fp.get('processorResponseCode')(transaction);;
+fp.pick(['uuid', 'locale', 'email', 'ip_address', 'country_code'])(user);;
+stripe.tokens.create({
+    card: fp.defaults({
+      exp_month: 12,
+      exp_year: new Date().getFullYear() + 3,
+      cvc: '123',
+      name: 'Payment Service Integration Test',
+    })(paymentDetails);,
+  });
+fp.omit(['type', 'stack'])(errorProperties);;
+fp.omit(['type', 'stack'])(errorProperties);
+fp.upperFirst(_.camelCase(type));
+fp.isNaN(exchangeRate);
+fp.intersection(userIdentifiers)(safeListedUserIdentifiers);;
+fp.invoke('toUpperCase')(c);;
+fp.cloneDeep(obj)
+fp.noop
---
+++ packages/change-codemod/transforms/convert-lodash-fp-to-curried-syntax.js
@@ -0,0 +1,28 @@
+// Convert lodash fp to curried syntax
+// This will apply to all usage with two arguments.
+//
+// Known issues: Unfortunately expressions will have semicolons in unfortunate
+// and syntactically incorrect places which need to be cleaned up manually
+// (text search and replace works for most cases).
+export default function transformer(file, api, options) {
+  const j = api.jscodeshift;
+  const root = j(file.source);
+
+  function convertToCurriedSyntax(node) {
+    if (node.value && node.value.arguments.length <= 1) return;
+    const otherArgs = node.value.arguments.splice(1, 10);
+    node.replace(j.expressionStatement(j.callExpression(node.value, otherArgs)));
+  }
+
+  root
+    .find(j.CallExpression, {
+      callee: {
+        object: {
+          name: "fp"
+        }
+      }
+    })
+    .forEach(convertToCurriedSyntax);
+
+  return root.toSource();
+}
